41f5072e2cf055ffe20056a72d8e5fa51d1420d1
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:23:12 +0530

    Create Assignment 7.3.C

---
+++ Assignment 7.3.C
@@ -0,0 +1,54 @@
+Q. 3. Write a program to print the first 10 natural numbers in reverse order
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=10;
+  while(a>=1) 
+  {
+    printf("%d\n",a);
+    a--;
+  }
+  
+    return 0;
+}
+
+
+
+
+RUN...... 
+
+
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+[Process completed - press Enter]
+
+
+
+
+
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=10) 
+  {
+    printf("%d\n",11-a);
+    a++;
+  }
+  
+    return 0;
+}
