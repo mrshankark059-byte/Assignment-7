0dc94d1399ef2fa4c0941931307b91e831e7f169
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:26:25 +0530

    Create Assignment 7.4.C

---
+++ Assignment 7.4.C
@@ -0,0 +1,32 @@
+Q. 4 Write a program to print the first 10 odd natural numbers
+
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=20) 
+  {
+    printf("%d\n",a);
+    a=a+2;
+  }
+  
+    return 0;
+}
+
+
+RUN.... 
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
+[Process completed - press Enter]
