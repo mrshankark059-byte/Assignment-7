7cc0c42dc88b162276ffc0ad86d6b641372179a7
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:47:32 +0530

    Create Assignment 7.9.C

---
+++ Assignment 7.9.C
@@ -0,0 +1,31 @@
+Q. 9 Write a program to print cubes of the first 10 natural numbers
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=10) 
+  {
+    printf("%d\n",a*a*a);
+    a++;
+  }
+  
+    return 0;
+}
+
+
+RUN..... 
+. 
+1
+8
+27
+64
+125
+216
+343
+512
+729
+1000
+
+[Process completed - press Enter]
