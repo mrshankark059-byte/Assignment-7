f8c7897320a88da976a7476133f81f217a52d059
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:35:50 +0530

    Create Assignment 7.6.C

---
+++ Assignment 7.6.C
@@ -0,0 +1,30 @@
+Q. 6. Write a program to print the first 10 even natural numbers
+
+#include <stdio.h>
+int main()
+{ 
+  int a=2;
+  while(a<=20) 
+  {
+    printf("%d\n",a);
+    a=a+2;
+  }
+  
+    return 0;
+}
+
+Run... 
+
+
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+
+[Process completed - press Enter]
