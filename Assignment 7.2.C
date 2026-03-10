eccc20eb38a12a72297efe238803ca7ea813152b
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:19:12 +0530

    Create Assignment 7.2.C

---
+++ Assignment 7.2.C
@@ -0,0 +1,36 @@
+
+Q. 2. Write a program to print the first 10 natural numbers.
+
+
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=10) 
+  {
+    printf("%d\n",a);
+    a++;
+  }
+  
+    return 0;
+}
+
+
+
+RUN
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+[Process completed - press Enter]
