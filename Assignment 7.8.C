312cf52696965da2c6991d10eaf7031e76d22010
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:44:47 +0530

    Create Assignment 7.8.C

---
+++ Assignment 7.8.C
@@ -0,0 +1,33 @@
+Q. 8.Write a program to print squares of the first 10 natural numbers. 
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=10) 
+  {
+    printf("%d\n",a*a);
+    a++;
+  }
+  
+    return 0;
+}
+
+RUN..... 
+
+
+
+
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
+
+[Process completed - press Enter]
