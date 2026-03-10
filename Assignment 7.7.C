0d81e8cda6f41f7a3d016676d26ec354437753f9
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:39:46 +0530

    Create Assignment 7.7.C

---
+++ Assignment 7.7.C
@@ -0,0 +1,30 @@
+Q. 7. Write a program to print the first 10 even natural numbers in reverse order
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=20;
+  while(a>=2) 
+  {
+    printf("%d\n",a);
+    a=a-2;
+  }
+  
+    return 0;
+}
+
+run... 
+
+20
+18
+16
+14
+12
+10
+8
+6
+4
+2
+
+[Process completed - press Enter]
