18c7046eb7d4029761b6d872c3540d3a01194757
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-10 09:12:41 +0530

    Create Assignment :- 7.1.C

---
+++ Assignment :- 7.1.C
@@ -0,0 +1,30 @@
+
+Q. 1. Write a program to print MySirG 5 times on the screen. 
+
+
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1;
+  while(a<=5) 
+  {
+   printf("MysirG\n");
+   a++;
+  }
+  
+    return 0;
+}
+
+
+RUN-----
+
+
+
+MysirG
+MysirG
+MysirG
+MysirG
+MysirG
+
+[Process completed - press Enter]
